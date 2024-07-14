15f6004a16d1ea964bd601a1470890676d8ffeb2
Rohith  Kanna <rohithkannakarunakaran@gmail.com>
2024-07-14 21:02:55 +0200

    Update and rename car.sql.txt to car.sql

---
+++ Car-dealer-sql/scripts/car.sql
@@ -0,0 +1,29 @@
+--Create table 
+
+CREATE TABLE dealer (
+dealer_id INT , 
+dealer_name VARCHAR(50),
+PRIMARY KEY (dealer_ID)
+);
+
+CREATE TABLE CAR (
+car_ID INT,
+Name VARCHAR(50),
+year INT,
+selling_price INT,
+mileage VARCHAR(50),
+dealer_ID INT,
+PRIMARY KEY (car_ID),
+FOREIGN KEY (dealer_ID) REFERENCES dealer(dealer_ID)
+);
+
+CREATE TABLE sales (
+car_ID INT,
+dealer_ID INT,
+selling_price INT,
+seller_type VARCHAR(50),
+FOREIGN KEY (dealer_ID) REFERENCES dealer(dealer_ID),
+FOREIGN KEY (car_ID) REFERENCES CAR(car_ID),
+);
+
+#loaded csv file using mysql workbench
